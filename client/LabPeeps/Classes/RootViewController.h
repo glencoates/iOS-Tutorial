dbacac5505805abd4063c614676b7e24012c1458
Glen Coates <glen@handshake-app.com>
2011-07-19 19:08:49 -0400

    This is the basic iOS project that XCode spits out for a navigation-based project with CoreData.  Committing so our changes are easier to diff.

---
+++ client/LabPeeps/Classes/RootViewController.h
@@ -0,0 +1,22 @@
+//
+//  RootViewController.h
+//  LabPeeps
+//
+//  Created by Glen Coates on 7/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
